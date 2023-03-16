a91324c7b59b427c2cbfd468311261399a47c819
cassiewatsonn <121472771+cassiewatsonn@users.noreply.github.com>
2023-03-16 18:24:44 -0400

    pushing before trying to run

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -21,7 +21,7 @@ module.exports = () => {
             // Webpack plugin that generates our html file and injects our bundles. 
             new HtmlWebpackPlugin({
               template: './index.html',
-              title: 'J.A.T.E'
+              title: 'anotherTextEditor'
             }),
            
             // Injects our custom service worker
