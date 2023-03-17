e236015957f8211c0caddd21a47bc85d250ed4de
cassiewatsonn <121472771+cassiewatsonn@users.noreply.github.com>
2023-03-17 15:35:30 -0400

    update webpack

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -50,9 +50,9 @@ module.exports = () => {
                   src: path.resolve('src/images/logo.png'),
                   sizes: [96, 128, 192, 256, 384, 512],
                   destination: path.join('assets', 'icons'),
-                    ios: true, 
-                    purpose: 'any maskable', 
-                    rename: 'icon-[width].[ext]'
+                    // ios: true, 
+                    // purpose: 'any maskable', 
+                    // rename: 'icon-[width].[ext]'
                 },
               ],
             }),
